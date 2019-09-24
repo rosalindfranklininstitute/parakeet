1770cd86592030efb97b4cfaa39fd78826445794
James Parkhurst <james.parkhurst@diamond.ac.uk>
2019-09-24 14:15:02 +0100

    Create a scan class to hold angle and translation info

---
+++ src/elfantasma/scan.py
@@ -0,0 +1,44 @@
+#
+# elfantasma.scan.py
+#
+# Copyright (C) 2019 Diamond Light Source
+#
+# Author: James Parkhurst
+#
+# This code is distributed under the GPLv3 license, a copy of
+# which is included in the root directory of this package.
+#
+
+
+class Scan(object):
+    """
+    A class to encapsulate an scan
+
+    """
+
+    def __init__(self, axis, angles):
+        """
+        Initialise the scan
+
+        Args:
+            axis (tuple): The rotation axis
+            angles (list): The rotation angles (units: degrees)
+
+        """
+        self.axis = axis
+        self.angles = angles
+
+
+def create_scan(
+    scan_type="still", axis=(1, 0, 0), start_angle=0, stop_angle=0, step_angle=0
+):
+    """
+    Create an scan
+
+    """
+    if scan_type == "still":
+        return Scan(axis=(1, 0, 0), angles=[0])
+    elif scan_type == "tilt_series":
+        return Scan(
+            axis=axis, angles=numpy.arange(start_angle, stop_angle, step_angle)
+        )
