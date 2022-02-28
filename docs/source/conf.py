8f5ef1a9e0bd2f4f168aa82ba1739cd3fb890b38
James Parkhurst <james.parkhurst@diamond.ac.uk>
2022-02-28 11:23:59 +0000

    Update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2022, James Parkhurst"
 author = "James Parkhurst"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.6"
+release = "0.2.7"
 
 
 # -- General configuration ---------------------------------------------------
