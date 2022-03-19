4b8ce27c8d0687b8b847d5eb69467cac1b72edf5
James Parkhurst <james.parkhurst@diamond.ac.uk>
2022-03-19 11:58:41 +0000

    Fixed import

--- src/parakeet/analyse/__init__.py
+++ src/parakeet/analyse/__init__.py
@@ -2,7 +2,7 @@
 from parakeet.analyse.correct import correct # noqa
 from parakeet.analyse.reconstruct import reconstruct # noqa
 from parakeet.analyse.average_particles import average_particles # noqa
-from parakeet.analyse.average_all_particles import average_all_particles # noqa
+from parakeet.analyse.average_particles import average_all_particles # noqa
 from parakeet.analyse.extract import extract # noqa
 from parakeet.analyse.refine import refine # noqa
 # fmt: on
