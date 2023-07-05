7093896f9954d58a056830c9ef9edae592632b0f
James Parkhurst <james.parkhurst@diamond.ac.uk>
2023-07-05 12:44:17 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ def main():
             "pandas",
             "pillow",
             "profet",
-            "pydantic=1.10.11",
+            "pydantic==1.10.11",
             "python-multem",
             "pyyaml",
             "scipy",
